ec4905718bc4185c3ede27407134f1f0950920fd
Cristhian304 <cristhianazocar10@gmail.com>
2025-06-04 22:14:48 -0300

    pruebo el programa, edito texto y estilos

--- App.js
+++ App.js
@@ -4,17 +4,19 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola profe ruiz</Text>
       <StatusBar style="auto" />
+      
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#707007',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    },
 });
