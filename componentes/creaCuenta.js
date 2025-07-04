d7857713b257b0febd8a70fb34877200e1a2c60c
Cristhian304 <cristhianazocar10@gmail.com>
2025-07-04 12:09:28 -0300

    pantalla crear cuenta y inicio sesion

--- componentes/creaCuenta.js
+++ componentes/creaCuenta.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
-export default function App() {
+export default function CrearCuenta() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Recetario</Text>
